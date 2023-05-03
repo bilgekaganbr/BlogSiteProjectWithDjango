e8ff5be9478d4a6e48acc135b1da1f58b348d0cf
Bilge Kağan Börekçi <bilgekaganbr@gmail.com>
2023-05-04 00:29:18 +0300

    BlogProj

--- BKBBlog/urls.py
+++ BKBBlog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#importing necessary modules and packages
+# Importing necessary modules and packages
 from django.contrib import admin
 from django.urls import path
 from article import views
--- article/models.py
+++ article/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-#defining the Article model
+# Defining the Article model
 class Article(models.Model):
 
-    #defining the author of the article as a foreign key to the User model
+    # Defining the author of the article as a foreign key to the User model
     author = models.ForeignKey("auth.User", on_delete = models.CASCADE)
-    #defining the title of the article as a character field with maximum length of 50 characters
+    # Defining the title of the article as a character field with maximum length of 50 characters
     title = models.CharField(max_length = 50)
-    #defining the content of the article as a text field
+    # Defining the content of the article as a text field
     content = models.TextField()
-    #defining the created date of the article as a date time field that automatically adds the current date and time
+    # Defining the created date of the article as a date time field that automatically adds the current date and time
     created_date = models.DateTimeField(auto_now_add = True)
 
-    #defining the string representation of the article
+    # Defining the string representation of the article
     def __str__(self):
 
         return self.title
